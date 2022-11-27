64241a761bcffe933b3f03ee1b567815f3d11369
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-11-27 05:09:23 +0300

    add splitbee

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/bee.js",
+        destination: "https://cdn.splitbee.io/sb.js",
+      },
+      {
+        source: "/_hive/:slug",
+        destination: "https://hive.splitbee.io/:slug",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
