0390c088926add964a3a124d32519eeae3229498
Samiul Haque Sohan <samiulhsohan@gmail.com>
2022-11-27 05:08:58 +0300

    add splitbee

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import splitbee from "@splitbee/web";
 
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "/bee.js",
+      apiUrl: "/_hive",
+    });
+  }, []);
+
   return <Component {...pageProps} />;
 }
